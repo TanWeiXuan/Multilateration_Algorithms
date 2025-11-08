db215c09339e2a53299708e76d04e6fdc95640b5
WeiXuan <tanweixuan999@gmail.com>
2025-11-08 21:07:46 +0800

    Added print helper for TestParameters

--- src/test_helpers.cpp
+++ src/test_helpers.cpp
@@ -1,8 +1,35 @@
 #include "test_helpers.h"
 
+#include <iostream>
+#include <format>
+
 std::mt19937_64 makeRandomEngine(std::optional<uint64_t> seed)
 {
     return std::mt19937_64(seed.value_or(std::random_device{}()));
+}
+
+void printTestParams(const TestParameters& params)
+{
+    std::cout << "Test Parameters:\n";
+    std::cout << std::format("  True Position: [{:.2f}, {:.2f}, {:.2f}]\n",  params.truePosition.x(),  params.truePosition.y(),  params.truePosition.z());
+
+    std::cout << "  Anchor Positions:\n";
+    for(size_t i = 0; i < params.anchorPositions.size(); ++i)
+    {
+        const Eigen::Vector3d& pos = params.anchorPositions[i];
+        std::cout << std::format("    {}: [{:.2f}, {:.2f}, {:.2f}]\n", i, pos.x(), pos.y(), pos.z());
+    }
+
+    std::cout << std::format("  Range Noise Std Dev: {:.2f}\n", params.rangeNoiseStdDev);
+
+    if(params.randomSeed.has_value())
+    {
+        std::cout << std::format("  Random Seed: {}\n", params.randomSeed.value());
+    }
+    else
+    {
+        std::cout << "  Random Seed: Not specified, using std::random_device\n";
+    }
 }
 
 // END OF FILE //
--- src/test_helpers.h
+++ src/test_helpers.h
@@ -7,6 +7,10 @@
 
 #include <Eigen/Dense>
 
+#include "tests.h"
+
 std::mt19937_64 makeRandomEngine(std::optional<uint64_t> seed);
+
+void printTestParams(const TestParameters& params);
 
 // END OF FILE //
