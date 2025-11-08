5921088402e0e644342a44953583d1414b088679
WeiXuan <tanweixuan999@gmail.com>
2025-11-08 19:26:43 +0800

    Added makeRandomEngine function

--- src/test_helpers.cpp
+++ src/test_helpers.cpp
@@ -1,3 +1,8 @@
 #include "test_helpers.h"
 
+std::mt19937_64 makeRandomEngine(std::optional<uint64_t> seed)
+{
+    return std::mt19937_64(seed.value_or(std::random_device{}()));
+}
+
 // END OF FILE //
--- src/test_helpers.h
+++ src/test_helpers.h
@@ -1,6 +1,12 @@
 #pragma once
 
+#include <random>
+#include <vector>
+#include <optional>
+#include <cstdint>
 
+#include <Eigen/Dense>
 
+std::mt19937_64 makeRandomEngine(std::optional<uint64_t> seed);
 
 // END OF FILE //
