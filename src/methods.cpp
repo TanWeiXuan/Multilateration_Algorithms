b1ef1419c21c9d88efa4f9c5d535ae7e0bcbab0d
WeiXuan <tanweixuan999@gmail.com>
2025-11-08 19:27:41 +0800

    Added helperfunction sq() and sumOver() to methods.cpp

--- src/methods.cpp
+++ src/methods.cpp
@@ -1,3 +1,28 @@
 #include "methods.h"
 
+#include <functional>
+
+namespace // anonymous namespace for helper functions
+{
+    template<typename T>
+    T sq(const T& x)
+    {
+        return x * x;
+    }
+
+    template<typename VecType, typename RetType = VecType>
+    RetType sumOver(
+        const std::vector<VecType>& vec, 
+        const std::function<RetType(const VecType&)>& fn = [](const VecType& x){ return x; }
+    )
+    {
+        RetType sum = RetType(0);
+        for(const VecType& v : vec)
+        {
+            sum += fn(v);
+        }
+        return sum;
+    }
+} // namespace anonymous
+
 // END OF FILE //
