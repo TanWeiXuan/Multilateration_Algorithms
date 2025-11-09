06d269264f190977d358f5d4ab491da0b45912b3
WeiXuan <tanweixuan999@gmail.com>
2025-11-09 16:36:23 +0800

    Documentation for test parameters

--- src/tests.h
+++ src/tests.h
@@ -7,12 +7,20 @@
 
 #include <Eigen/Dense>
 
-struct TestParameters
-{
+struct TestParameters {
+    // Position of the target to be estimated
     Eigen::Vector3d truePosition = Eigen::Vector3d::Zero();
+
+    // Positions of the anchors
     std::vector<Eigen::Vector3d> anchorPositions = {};
+
+    // Standard deviation of the range noise
     double rangeNoiseStdDev = 0.0;
+
+    // Random seed for noise and outliers
     std::optional<uint64_t> randomSeed = std::nullopt;
+
+    // Number of runs per test
     size_t numRuns = 1;
 };
 
